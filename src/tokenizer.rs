4f8e7415fae110c8502ea629f7c879a631ff3c7a
Mahesh Bansod <coolkevin011@gmail.com>
2024-09-22 01:21:19 +0530

    tok: change string literal tok logic

--- src/tokenizer.rs
+++ src/tokenizer.rs
@@ -6,6 +6,8 @@ pub enum TokenType {
     Identifier,
     Let,
     Literal,
+    StringLiteralStart,
+    StringLiteralEnd,
     Minus,
     Plus,
     Semicolon,
@@ -38,11 +40,26 @@ impl<'a> Token<'a> {
     }
 }
 
+#[derive(Clone, Copy, PartialEq)]
+enum Delimeter {
+    DoubleQuotes,
+    SingleQuote,
+}
+
+#[derive(Clone, Copy)]
+enum TokenizationMode {
+    Normal,
+    String { delimeter: Delimeter },
+}
+
 pub struct Tokenizer<'a> {
     code: &'a str,
     char_indices: CharIndices<'a>,
     current_line: usize,
     current_line_first_index: usize,
+    /// Stack of modes
+    /// When the tokenizer is within a string or come out of it, it'll store the mode here
+    mode_stack: Vec<TokenizationMode>,
 }
 
 impl<'a> Tokenizer<'a> {
@@ -52,7 +69,21 @@ impl<'a> Tokenizer<'a> {
             char_indices: code.char_indices(),
             current_line: 1,
             current_line_first_index: 0,
+
+            mode_stack: vec![],
         }
+    }
+
+    fn mode(&self) -> TokenizationMode {
+        *self.mode_stack.last().unwrap_or(&TokenizationMode::Normal)
+    }
+
+    fn set_mode(&mut self, mode: TokenizationMode) {
+        self.mode_stack.push(mode);
+    }
+
+    fn pop_mode(&mut self) -> Option<TokenizationMode> {
+        self.mode_stack.pop()
     }
 
     fn consume_while<C>(&mut self, condition: C) -> Option<&'a str>
@@ -192,30 +223,51 @@ impl<'a> Tokenizer<'a> {
                     Tokenizer::consume_while_it(&self.char_indices, |c| c.is_numeric()).unwrap();
                 Some(self.match_token(it, TokenType::Literal, first_index, last_index))
             }
-            Some((first_index, c)) if c == '\'' || c == '"' => {
-                let quote = c;
-                // implementing super simple naive algo
-
-                // consume this one
-                self.char_indices.next().expect("is the quote");
-                let mut it = if let Some((_, _, it)) =
-                    Tokenizer::consume_while_it(&self.char_indices, |c| c != quote)
-                {
-                    it
-                } else {
-                    // empty string
-                    self.char_indices.clone()
-                };
-
-                let (last_index, _) = it
-                    .next()
-                    .expect("todo: should propagate an error of missing end quote");
-                // consuming end quote
-                let literal = self.match_token(it, TokenType::Literal, first_index, last_index);
-                Some(literal)
+            Some((first_index, c)) if is_quote(c) => {
+                let delimeter = quote_as_delimeter(c).expect("already checked above");
+                self.char_indices = it_clone;
+                self.set_mode(TokenizationMode::String { delimeter });
+                Some(self.match_token(
+                    self.char_indices.clone(),
+                    TokenType::StringLiteralStart,
+                    first_index,
+                    first_index,
+                ))
             }
             _ => None,
         }
+    }
+
+    fn consume_string(&mut self, delimeter: Delimeter) -> Option<Token<'a>> {
+        if let Some((first_index, last_index, it)) =
+            Tokenizer::consume_while_it(&self.char_indices, |c| !is_quote(c))
+        {
+            Some(self.match_token(it, TokenType::Literal, first_index, last_index))
+        } else {
+            let mut it_clone = self.char_indices.clone();
+            if let Some((i, next_char)) = it_clone.next() {
+                if let Some(expected_delimeter) = quote_as_delimeter(next_char) {
+                    if expected_delimeter == delimeter {
+                        let token = self.match_token(it_clone, TokenType::StringLiteralEnd, i, i);
+                        self.pop_mode();
+                        return Some(token);
+                    }
+                }
+            }
+            None
+        }
+    }
+}
+
+fn is_quote(c: char) -> bool {
+    c == '\'' || c == '"'
+}
+
+fn quote_as_delimeter(c: char) -> Option<Delimeter> {
+    match c {
+        '\'' => Some(Delimeter::SingleQuote),
+        '"' => Some(Delimeter::DoubleQuotes),
+        _ => None,
     }
 }
 
@@ -223,12 +275,16 @@ impl<'a> Iterator for Tokenizer<'a> {
     type Item = Token<'a>;
 
     fn next(&mut self) -> Option<Self::Item> {
-        self.consume_whitespace();
-        return self
-            .try_consume_keyword()
-            .or_else(|| self.try_consume_operator())
-            .or_else(|| self.try_consume_identifier())
-            .or_else(|| self.try_consume_literal());
+        match self.mode() {
+            TokenizationMode::Normal => {
+                self.consume_whitespace();
+                self.try_consume_keyword()
+                    .or_else(|| self.try_consume_operator())
+                    .or_else(|| self.try_consume_identifier())
+                    .or_else(|| self.try_consume_literal())
+            }
+            TokenizationMode::String { delimeter } => self.consume_string(delimeter),
+        }
     }
 }
 
@@ -338,35 +394,65 @@ third = first + second;
     fn it_should_tokenize_string_literal() {
         let code = "'1'";
         let mut tokenizer = Tokenizer::new(code);
-        let next = tokenizer.next().expect("Literal exists");
-        let expected = Token::new(
-            TokenType::Literal,
-            TokenLocation { row: 1, column: 1 },
-            "'1'",
-        );
-        assert_eq!(expected, next);
-        let code = "\"1\"";
+        let expected_tokens = vec![
+            Token::new(
+                TokenType::StringLiteralStart,
+                TokenLocation { row: 1, column: 1 },
+                "'",
+            ),
+            Token::new(TokenType::Literal, TokenLocation { row: 1, column: 2 }, "1"),
+            Token::new(
+                TokenType::StringLiteralEnd,
+                TokenLocation { row: 1, column: 3 },
+                "'",
+            ),
+        ];
+        for expected in expected_tokens {
+            assert_eq!(expected, tokenizer.next().unwrap());
+        }
+        let code = "\"1 abcd let const lets go\"";
         let mut tokenizer = Tokenizer::new(code);
-        let next = tokenizer.next().expect("Literal exists");
-        let expected = Token::new(
-            TokenType::Literal,
-            TokenLocation { row: 1, column: 1 },
-            "\"1\"",
-        );
-        assert_eq!(expected, next);
+        let expected_tokens = vec![
+            Token::new(
+                TokenType::StringLiteralStart,
+                TokenLocation { row: 1, column: 1 },
+                "\"",
+            ),
+            Token::new(
+                TokenType::Literal,
+                TokenLocation { row: 1, column: 2 },
+                "1 abcd let const lets go",
+            ),
+            Token::new(
+                TokenType::StringLiteralEnd,
+                TokenLocation { row: 1, column: 26 },
+                "\"",
+            ),
+        ];
+        for expected in expected_tokens {
+            assert_eq!(expected, tokenizer.next().unwrap());
+        }
     }
 
     #[test]
     fn it_should_tokenize_empty_string_literal() {
         let code = "''";
         let mut tokenizer = Tokenizer::new(code);
-        let next = tokenizer.next().expect("Literal exists");
-        let expected = Token::new(
-            TokenType::Literal,
-            TokenLocation { row: 1, column: 1 },
-            "''",
-        );
-        assert_eq!(expected, next);
+        let expected_tokens = vec![
+            Token::new(
+                TokenType::StringLiteralStart,
+                TokenLocation { row: 1, column: 1 },
+                "'",
+            ),
+            Token::new(
+                TokenType::StringLiteralEnd,
+                TokenLocation { row: 1, column: 2 },
+                "'",
+            ),
+        ];
+        for token in expected_tokens {
+            assert_eq!(token, tokenizer.next().unwrap());
+        }
     }
 
     #[test]
