46bc60101465cee89902a5451dbb669cfc839c38
Mahesh Bansod <coolkevin011@gmail.com>
2024-09-27 22:50:44 +0530

    parser: expression as object key

--- src/parser/object.rs
+++ src/parser/object.rs
@@ -33,11 +33,18 @@ impl<'a> Parser<'a> {
     }
 
     fn key_value(&mut self) -> ParseResult<'a, PKeyValue<'a>> {
-        let identifier = self.parse_identifier()?;
+        let key = if self.expect_token(TokenType::SquareBracketOpen).is_ok() {
+            let expression = self.parse_expression()?;
+            self.expect_token(TokenType::SquareBracketClose)?;
+            PObjectKey::Expression(expression)
+        } else {
+            let identifier = self.parse_identifier()?;
+            PObjectKey::Identifier(identifier)
+        };
         self.expect_token(TokenType::Colon)?;
         let expression = self.parse_expression()?;
         Ok(PKeyValue {
-            key: PObjectKey::Identifier(identifier),
+            key,
             value: expression,
         })
     }
@@ -253,6 +260,67 @@ let code = {
                                 ),
                             }),
                         ))],
+                    }))),
+                }],
+            },
+        };
+        assert_eq!(expected_tree, tree);
+
+        Ok(())
+    }
+
+    #[test]
+    fn expression_key<'a>() -> ParseResult<'a, ()> {
+        let (tree, errors) = parse_code(
+            "
+let code = {
+    ['key']: 1
+}
+",
+        )?;
+        assert_eq!(errors, vec![]);
+        let expected_tree = ParseTree {
+            root: ParseTreeRoot {
+                statements: vec![PStatement::Binding {
+                    binding_type: BindingType::Let,
+                    identifier: PIdentifier {
+                        token: Token::new(
+                            TokenType::Identifier,
+                            TokenLocation { row: 2, column: 5 },
+                            "code",
+                        ),
+                    },
+                    value: Some(PExpression::Atom(PAtom::ObjectLiteral(PObject {
+                        entries: vec![PObjectEntry::KeyValue(PKeyValue {
+                            key: PObjectKey::Expression(PExpression::Atom(PAtom::Literal(
+                                PLiteralPrimitive::String {
+                                    value_token: Some(Token::new(
+                                        TokenType::Literal,
+                                        TokenLocation { row: 3, column: 7 },
+                                        "key",
+                                    )),
+                                    value: "key",
+                                    start_delim: Token::new(
+                                        TokenType::StringLiteralStart,
+                                        TokenLocation { row: 3, column: 6 },
+                                        "'",
+                                    ),
+                                    end_delim: Token::new(
+                                        TokenType::StringLiteralEnd,
+                                        TokenLocation { row: 3, column: 10 },
+                                        "'",
+                                    ),
+                                },
+                            ))),
+                            value: PExpression::Atom(PAtom::Literal(PLiteralPrimitive::Number {
+                                value: 1.0,
+                                token: Token::new(
+                                    TokenType::Literal,
+                                    TokenLocation { row: 3, column: 14 },
+                                    "1",
+                                ),
+                            })),
+                        })],
                     }))),
                 }],
             },
