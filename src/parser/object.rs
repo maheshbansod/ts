1b2f91aa8753e84a4efc38b3e80db34ae8c4fcc7
Mahesh Bansod <coolkevin011@gmail.com>
2024-09-27 21:35:39 +0530

    parser: multiple key value pairs in object

--- src/parser/object.rs
+++ src/parser/object.rs
@@ -4,18 +4,34 @@ use super::{PKeyValue, PObject, PObjectEntry, PObjectKey, ParseResult, Parser};
 
 impl<'a> Parser<'a> {
     pub(super) fn parse_object(&mut self) -> ParseResult<'a, PObject<'a>> {
+        let entries = self.object_entries()?;
+        self.expect_token(TokenType::BraceClose)?;
+        let _ = self.expect_token(TokenType::Comma);
+        let object = PObject { entries };
+        Ok(object)
+    }
+
+    fn object_entries(&mut self) -> ParseResult<'a, Vec<PObjectEntry<'a>>> {
+        let mut object_entries = vec![];
+        loop {
+            let key_value = self.key_value()?;
+            let key_value = PObjectEntry::KeyValue(key_value);
+            object_entries.push(key_value);
+            if self.expect_token(TokenType::Comma).is_err() {
+                break;
+            }
+        }
+        Ok(object_entries)
+    }
+
+    fn key_value(&mut self) -> ParseResult<'a, PKeyValue<'a>> {
         let identifier = self.parse_identifier()?;
         self.expect_token(TokenType::Colon)?;
         let expression = self.parse_expression()?;
-        let _ = self.expect_token(TokenType::Comma);
-        self.expect_token(TokenType::BraceClose)?;
-        let object = PObject {
-            entries: vec![PObjectEntry::KeyValue(PKeyValue {
-                key: PObjectKey::Identifier(identifier),
-                value: expression,
-            })],
-        };
-        Ok(object)
+        Ok(PKeyValue {
+            key: PObjectKey::Identifier(identifier),
+            value: expression,
+        })
     }
 }
 
@@ -69,6 +85,78 @@ let code = {
                                 ),
                             })),
                         })],
+                    }))),
+                }],
+            },
+        };
+        assert_eq!(expected_tree, tree);
+
+        Ok(())
+    }
+
+    #[test]
+    fn multiple_keys<'a>() -> ParseResult<'a, ()> {
+        let (tree, errors) = parse_code(
+            "
+let code = {
+    a: 1,
+    b: 3
+}
+",
+        )?;
+        assert_eq!(errors, vec![]);
+        let expected_tree = ParseTree {
+            root: ParseTreeRoot {
+                statements: vec![PStatement::Binding {
+                    binding_type: BindingType::Let,
+                    identifier: PIdentifier {
+                        token: Token::new(
+                            TokenType::Identifier,
+                            TokenLocation { row: 2, column: 5 },
+                            "code",
+                        ),
+                    },
+                    value: Some(PExpression::Atom(PAtom::ObjectLiteral(PObject {
+                        entries: vec![
+                            PObjectEntry::KeyValue(PKeyValue {
+                                key: PObjectKey::Identifier(PIdentifier {
+                                    token: Token::new(
+                                        TokenType::Identifier,
+                                        TokenLocation { row: 3, column: 5 },
+                                        "a",
+                                    ),
+                                }),
+                                value: PExpression::Atom(PAtom::Literal(
+                                    PLiteralPrimitive::Number {
+                                        value: 1.0,
+                                        token: Token::new(
+                                            TokenType::Literal,
+                                            TokenLocation { row: 3, column: 8 },
+                                            "1",
+                                        ),
+                                    },
+                                )),
+                            }),
+                            PObjectEntry::KeyValue(PKeyValue {
+                                key: PObjectKey::Identifier(PIdentifier {
+                                    token: Token::new(
+                                        TokenType::Identifier,
+                                        TokenLocation { row: 4, column: 5 },
+                                        "b",
+                                    ),
+                                }),
+                                value: PExpression::Atom(PAtom::Literal(
+                                    PLiteralPrimitive::Number {
+                                        value: 3.0,
+                                        token: Token::new(
+                                            TokenType::Literal,
+                                            TokenLocation { row: 4, column: 8 },
+                                            "3",
+                                        ),
+                                    },
+                                )),
+                            }),
+                        ],
                     }))),
                 }],
             },
