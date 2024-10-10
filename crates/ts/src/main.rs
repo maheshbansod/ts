94bd33bb293593773899e2c2e7bd98e42bb22db9
Mahesh Bansod <coolkevin011@gmail.com>
2024-10-10 23:09:07 +0530

    add a hello world file

--- crates/ts/src/main.rs
+++ crates/ts/src/main.rs
@@ -1,4 +1,8 @@
-use std::error::Error;
+use std::{error::Error, fs::read_to_string};
+
+use checker::Checker;
+use parser::Parser;
+use tokenizer::Tokenizer;
 
 mod checker;
 mod interactive;
@@ -6,5 +10,41 @@ mod parser;
 mod tokenizer;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    interactive::interactive()
+    let args = std::env::args();
+    if args.len() == 1 {
+        interactive::interactive()
+    } else {
+        for arg in args.skip(1) {
+            let file_name = arg;
+            println!("Loading file '{file_name}'...");
+            let code = read_to_string(file_name)?; // todo use buffer
+            println!("Loaded code into memory ✅");
+            let tokenizer = Tokenizer::new(&code);
+            let parser = Parser::new(tokenizer);
+            println!("Parsing...");
+            match parser.parse() {
+                Ok((tree, errors)) => {
+                    if errors.is_empty() {
+                        println!("No parser errors ✅");
+                    } else {
+                        println!("❌ Parser errors: {errors:?}");
+                    }
+                    let checker = Checker::new(&tree);
+                    println!("Type checking...");
+                    let (errors, types) = checker.check();
+                    if errors.is_empty() {
+                        println!("No checker errors ✅");
+                    } else {
+                        println!("❌ Type checking Errors: {:?}", errors);
+                    }
+                    println!("Types: {:?}", types);
+                }
+                Err(err) => {
+                    println!("❌ Error while parsing");
+                    println!("{err:?}");
+                }
+            }
+        }
+        Ok(())
+    }
 }
